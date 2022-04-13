cb5cd75015dae98b058c2599374de03053283df3
Techoplite <oriccimirko@gmail.com>
2022-04-13 14:03:57 +0100

    Initialise project with mobile landing background

---
+++ pages/components/Section.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function section(props) {
+  return <section className={props.styleName}>
+    <div className={props.overlayName}></div>
+  </section>
+}
+
