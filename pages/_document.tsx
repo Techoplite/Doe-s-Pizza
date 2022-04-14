9f505defc215968353c38c47bfe0d25a0fc10ca3
Techoplite <oriccimirko@gmail.com>
2022-04-14 19:48:24 +0100

    Add business info to landing page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,14 +3,18 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
     render() {
         return (
-            <Html style={{padding: 0, margin: 0}}>
+            <Html style={{ padding: 0, margin: 0 }}>
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                     <link href="https://fonts.googleapis.com/css2?family=Arvo:wght@700&wght500&display=swap" rel="stylesheet" />
+
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                    <link href="https://fonts.googleapis.com/css2?family=Andika+New+Basic:wght@700&display=swap" rel="stylesheet" />
                 </Head>
-                <body style={{padding: 0, margin: 0}}>
-                    <Main/>
+                <body style={{ padding: 0, margin: 0 }}>
+                    <Main />
                     <NextScript />
                 </body>
             </Html>
--- pages/components/Landing.tsx
+++ pages/components/Landing.tsx
@@ -8,8 +8,13 @@ export default function Landing() {
     return (
         <section className={styles.sectionLanding}>
             <h1>LIKE MAMMA USED TO MAKE...</h1>
-                <LandingBtn label="Book Now" />
-                <LandingBtn label="Order Online*" />
+            <LandingBtn label="Book Now" />
+            <LandingBtn label="Order Online*" />
+            <div className="business-info">
+                <h2>Open from 11am to 11pm</h2>
+                <h2>Tuesday closed</h2>
+                <h3>*Account required</h3>
+            </div>
         </section>
 
 
