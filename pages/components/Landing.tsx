e1db5f24e490d9a0a4ff5381fb22dcd1d2315553
Techoplite <oriccimirko@gmail.com>
2022-04-14 20:18:30 +0100

    Add animated down chevs

--- pages/components/Landing.tsx
+++ pages/components/Landing.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from "../../styles/Landing.module.scss";
 import LandingBtn from './LandingBtn';
+import Image from 'next/image'
+
 
 
 
@@ -10,10 +12,19 @@ export default function Landing() {
             <h1>LIKE MAMMA USED TO MAKE...</h1>
             <LandingBtn label="Book Now" />
             <LandingBtn label="Order Online*" />
-            <div className="business-info">
+            <div>
                 <h2>Open from 11am to 11pm</h2>
                 <h2>Tuesday closed</h2>
                 <h3>*Account required</h3>
+            </div>
+            <div className={styles.downChevrons}>
+                <Image
+                    src="/../public/down-chevrons.png"
+                    alt="Landscape picture"
+                    width={30}
+                    height={30}
+
+                />
             </div>
         </section>
 
