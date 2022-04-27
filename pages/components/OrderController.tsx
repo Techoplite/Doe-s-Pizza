cb37a2b1d068c7410ec6faf933ac7b377f75791c
Techoplite <oriccimirko@gmail.com>
2022-04-27 13:08:52 +0100

    Refactor 'increase/decrease' quantity logic into single handler

--- pages/components/OrderController.tsx
+++ pages/components/OrderController.tsx
@@ -3,18 +3,28 @@ import styles from "../../styles/OrderController.module.scss";
 
 export default function OrderController() {
   const [quantity, setQuantity] = useState(0)
+  const handleClick: React.MouseEventHandler<HTMLParagraphElement> = (event) => {
+    switch ((event.target as HTMLParagraphElement).id) {
+      case 'decrease':
+        setQuantity((prevQuantity) => prevQuantity > 0 ? prevQuantity - 1 : 0)
+        break
+      case 'increase':
+        setQuantity((prevQuantity) => prevQuantity + 1)
+        break
+    }
+  }
   return (
     <div className={styles['container']}>
       <p
+        id='decrease'
         className={`${styles['grid-item']} ${styles['symbol']}`}
-        onClick={() => setQuantity(
-          (prevQuantity) => prevQuantity > 0 ? prevQuantity - 1 : 0)
-        }
+        onClick={handleClick}
       >-</p>
       <p className={styles['grid-item']}>{quantity}</p>
       <p
+        id='increase'
         className={`${styles['grid-item']} ${styles['symbol']}`}
-        onClick={() => setQuantity((prevQuantity) => prevQuantity + 1)}
+        onClick={handleClick}
       >+</p>
     </div>
   )
