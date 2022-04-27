1936848ddc0248e9bc43b2f97c1d9cad9c8f691e
Techoplite <oriccimirko@gmail.com>
2022-04-27 12:30:22 +0100

    Make menu pizzas dynamic and minor controller restyle

--- pages/components/MenuItem.tsx
+++ pages/components/MenuItem.tsx
@@ -5,27 +5,32 @@ import OrderController from './OrderController';
 
 
 
-export default function MenuItem() {
-    return (
-        <div className={styles['item-container']}>
-            <div className={styles['top']}>
-                <div className={styles['image']}>
-                <Image
-                    src="/../public/pizzas/margherita.png"
-                    alt="Landscape picture"
-                    width={150}
-                    height={150}
-                    />
-                    </div>
-                <div className={styles['details']}>
-                    <h1 className={styles['h1']}>Margherita</h1>
-                    <h2 className={styles['h2']}>(Translation)</h2>
-                    <p className={styles['p']}>Tomato Base, Mozzarella, Basil, test, test, test, test, test</p>
-                </div>
-            </div>
-            <div >
-                <OrderController />
-            </div>
+export default function MenuItem(props: {
+  imageSrc: string,
+  title: string
+  ingredients: string
+  price: string
+}) {
+  return (
+    <div className={styles['item-container']}>
+      <div className={styles['top']}>
+        <div className={styles['image']}>
+          <Image
+            src={props.imageSrc}
+            alt="Landscape picture"
+            width={150}
+            height={150}
+          />
         </div>
-    )
+        <div className={styles['details']}>
+          <h1 className={styles['h1']}>{props.title}</h1>
+          <h2 className={styles['h2']}>Price: £{parseFloat(props.price).toFixed(2)}</h2>
+          <p className={styles['p']}>{props.ingredients}</p>
+        </div>
+      </div>
+      <div >
+        <OrderController />
+      </div>
+    </div>
+  )
 }
--- pages/online-order/index.tsx
+++ pages/online-order/index.tsx
@@ -5,30 +5,42 @@ import Footer from '../components/Footer';
 import DefaultBtn from '../components/DefaultBtn';
 import { store } from '../redux/store'
 import { Provider } from 'react-redux'
-import OrderItem from '../components/MenuItem';
+import MenuItem from '../components/MenuItem';
 import Link from 'next/link'
+import { useAppSelector } from '../redux/hooks';
 
 
 
 export default function LogIn() {
-    return (
-        <Provider store={store}>
-            <div className={styles['container']}>
-                <Navbar />
-                <div className={styles['main-content']}>
-                    <OrderItem />
-                    <OrderItem />
-                    <OrderItem />
-                </div>
-                <Link href="/checkout" passHref >
-                    <div className={styles['btn']}>
-                        <DefaultBtn label="Checkout" />
-                    </div>
-                </Link>
-                <div className={styles['footer-wrapper']}>
-                    <Footer />
-                </div>
-            </div>
-        </Provider>
-    )
+  const pizzas = useAppSelector(state => state.pizzas)
+
+  return (
+    <Provider store={store}>
+      <div className={styles['container']}>
+        <Navbar />
+        <div className={styles['main-content']}>
+          <ul>
+            {pizzas.data.nodes && pizzas.data.nodes.map(pizza =>
+              <li key={pizza.title}>
+                <MenuItem
+                  imageSrc={pizza.featuredImage.node.sourceUrl}
+                  title={pizza.title}
+                  ingredients={pizza.data.ingredients}
+                  price={pizza.data.price}
+                />
+              </li>
+            )}
+          </ul>
+        </div>
+        <Link href="/checkout" passHref >
+          <div className={styles['btn']}>
+            <DefaultBtn label="Checkout" />
+          </div>
+        </Link>
+        <div className={styles['footer-wrapper']}>
+          <Footer />
+        </div>
+      </div>
+    </Provider>
+  )
 }
