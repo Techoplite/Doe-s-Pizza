6a55cd4e4b7b91026a9a45bee4c6fc1d03f191eb
Techoplite <oriccimirko@gmail.com>
2022-05-02 06:47:30 +0100

    fix src url

--- pages/components/Landing.tsx
+++ pages/components/Landing.tsx
@@ -26,7 +26,7 @@ export default function Landing(props) {
   return (
     <section className={styles['section-landing']}>
       <Image
-        src="/../public/background-images/landing.jpg"
+        src="/public/background-images/landing.jpg"
         alt="A pizza on a wooden tray"
         layout='fill'
         objectFit='cover'
