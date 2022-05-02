6184e26a8b0421b4a5dec07dab8d413d0fca10cb
Techoplite <oriccimirko@gmail.com>
2022-05-02 08:41:33 +0100

    Try fix for PWA

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')
+const runtimeCaching = require("next-pwa/cache");
 const nextConfig = {
     pwa: {
         dest: 'public',
         register: true,
         skipWaiting: true,
         disable: process.env.NODE_ENV === 'development',
+        buildExcludes: ['/middleware-manifest.json$/'],
+        runtimeCaching
     },
     reactStrictMode: true,
     images: {
