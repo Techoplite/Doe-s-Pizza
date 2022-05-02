3109b1154a06cf28a07688f13d54e6c2e194e9fe
Techoplite <oriccimirko@gmail.com>
2022-05-02 06:45:48 +0100

    Make landing bg image a next element

--- pages/components/Landing.tsx
+++ pages/components/Landing.tsx
@@ -10,51 +10,58 @@ import { styled } from '@mui/material/styles';
 
 // TODO: AlertDialog background color not working 
 const StyledDialog = styled(AlertDialog)({
-    '& .MuiDialog-paper': {
-        backgroundColor: yellow[200]
-    }
+  '& .MuiDialog-paper': {
+    backgroundColor: yellow[200]
+  }
 });
 
 export default function Landing(props) {
-    const router = useRouter();
-    const isAuthenticated = useAppSelector(state => state.auth.isAuthenticated)
-    const handleClick = () => {
-        isAuthenticated ? router.push('/online-order') : setOpenDialog(true)
-    }
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [openDialog, setOpenDialog] = useState(false)
-    return (
-        <section className={styles['section-landing']}>
-            <StyledDialog
-                openDialog={openDialog}
-                setOpenDialog={setOpenDialog}
-                title="Account Required"
-                message="You need an account in order to make an online order. Please Sign Up or Log In."
-                successRedirect={null}
-            />
-            <h1>LIKE MAMMA USED TO MAKE...</h1>
-            <LandingBtn label="Book Now" linkRef='/book-now' handler={null} />
-            <LandingBtn
-                label="Order Online*"
-                handler={handleClick}
-                linkRef={null}
-            />
-            <div className={styles.businessInfo}>
-                <h2>Open from 11am to 11pm</h2>
-                <h2>Tuesday closed</h2>
-                <h3>*Account required</h3>
-            </div>
-            <div
-                className={styles.downChevrons}
-                onClick={() => props.setScrollDown(true)}
-            >
-                <Image
-                    src="/down-chevrons.png"
-                    alt="Landscape picture"
-                    width={30}
-                    height={30}
-                />
-            </div>
-        </section>
-    )
+  const router = useRouter();
+  const isAuthenticated = useAppSelector(state => state.auth.isAuthenticated)
+  const handleClick = () => {
+    isAuthenticated ? router.push('/online-order') : setOpenDialog(true)
+  }
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [openDialog, setOpenDialog] = useState(false)
+  return (
+    <section className={styles['section-landing']}>
+      <Image
+        src="/../public/background-images/landing.jpg"
+        alt="A pizza on a wooden tray"
+        layout='fill'
+        objectFit='cover'
+        objectPosition='left'
+      />
+      <StyledDialog
+        openDialog={openDialog}
+        setOpenDialog={setOpenDialog}
+        title="Account Required"
+        message="You need an account in order to make an online order. Please Sign Up or Log In."
+        successRedirect={null}
+      />
+      <h1>LIKE MAMMA USED TO MAKE...</h1>
+      <LandingBtn label="Book Now" linkRef='/book-now' handler={null} />
+      <LandingBtn
+        label="Order Online*"
+        handler={handleClick}
+        linkRef={null}
+      />
+      <div className={styles.businessInfo}>
+        <h2>Open from 11am to 11pm</h2>
+        <h2>Tuesday closed</h2>
+        <h3>*Account required</h3>
+      </div>
+      <div
+        className={styles.downChevrons}
+        onClick={() => props.setScrollDown(true)}
+      >
+        <Image
+          src="/down-chevrons.png"
+          alt="Landscape picture"
+          width={30}
+          height={30}
+        />
+      </div>
+    </section>
+  )
 }
