32188133ba54575c4aeddff2c48279ad8f15f4e8
Techoplite <oriccimirko@gmail.com>
2022-05-08 09:01:27 +0100

    Add styling for about us on tablet

--- pages/components/AboutUs.tsx
+++ pages/components/AboutUs.tsx
@@ -5,37 +5,42 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 export default function AboutUs() {
-    useEffect(() => {
-        AOS.init({ duration: 1500 })
-    }, [])
-    return (
-        <section className={styles['section-about-us']} id='about'>
-            <div className={styles['top-spacer']}></div>
-            <div className={styles['main-content']}>
-                <h1>About Us</h1>
-                <p data-aos="fade-up">At Doe’s Pizza we do things properly.</p>
-                <Image
-                    data-aos="fade-up"
-                    src="/small/about-us-pic1.png"
-                    alt="A pizza in front of a wood fired oven"
-                    width={255.65}
-                    height={141.82}
-                    layout="responsive"
-                />
-                <p data-aos="fade-up">Our wood fired oven will give your pizza a puffy crust and make it taste the way it should.</p>
-                <Image
-                    data-aos="fade-up"
-                    src="/small/about-us-pic2.png"
-                    alt="A pizza in front of a wood fired oven"
-                    width={257}
-                    height={195.76}
-                    layout="responsive"
-                />
-                <p data-aos="zoom-up" data-aos-anchor-placement="top-bottom">
-                    We use only the best ingredients to deliver the best product.
-                </p>
-            </div>
-        </section>
+  useEffect(() => {
+    AOS.init({ duration: 1500 })
+  }, [])
+  return (
+    <section className={styles['section-about-us']} id='about'>
+      <div className={styles['top-spacer']}></div>
+      <div className={styles['main-content']}>
+        <h1>About Us</h1>
+        <p data-aos="fade-up">At Doe’s Pizza we do things properly.</p>
+        <div className={styles['wrapper']}>
+          <Image
+            data-aos="fade-up"
+            src="/small/about-us-pic1.png"
+            alt="A pizza in front of a wood fired oven"
+            width={255.65}
+            height={141.82}
+            layout="responsive"
+          />
+          <p data-aos="fade-up">Our wood fired oven will give your pizza a puffy crust and make it taste the way it should.</p>
+        </div>
+        <div className={styles['wrapper']}>
+          <Image
+            data-aos="fade-up"
+            src="/small/about-us-pic2.png"
+            alt="A pizza in front of a wood fired oven"
+            width={257}
+            height={195.76}
+            layout="responsive"
+          />
+          <p data-aos="zoom-up" data-aos-anchor-placement="top-bottom">
+            We use only the best ingredients to deliver the best product.
+          </p>
+        </div>
 
-    )
+      </div>
+    </section>
+
+  )
 }
