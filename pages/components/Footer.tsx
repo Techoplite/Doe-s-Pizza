edca28f017bfdb98735a39fbf3b15df7aeff1053
Techoplite <oriccimirko@gmail.com>
2022-05-09 11:07:31 +0100

    Ad auth links to footing

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -3,11 +3,33 @@ import styles from "../../styles/Footer.module.scss";
 import Image from 'next/image'
 import Link from 'next/link';
 import { toggle } from '../../redux/menu/menuSlice';
-import { useAppDispatch } from '../../redux/hooks';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { setIsAuthenticated } from '../../redux/auth/authSlice';
+import { resetBooking } from '../../redux/booking/bookingSlice';
+import { resetOrder } from '../../redux/order/orderSlice';
+import { resetYourOrders } from '../../redux/yourOrders/yourOrdersSlice';
 
 
 export default function Footer() {
   const dispatch = useAppDispatch()
+  const isAuthenticated = useAppSelector(state => state.auth.isAuthenticated)
+  const handleLogout = () => {
+    // Should reset every single store
+    dispatch(toggle({ section: 'landing', open: false }))
+    dispatch(setIsAuthenticated({
+      isAuthenticated: false, credentials: {
+        username: '',
+        password: ''
+      }
+    }))
+    dispatch(resetBooking())
+    dispatch(resetOrder())
+    dispatch(resetYourOrders())
+  }
+  const homeLink =
+    <Link href="/" passHref >
+      <p onClick={() => dispatch(toggle({ section: 'landing', open: false }))}>Home</p>
+    </Link>
   return (
     <section className={styles['section-footer']}>
       <div className={styles['logo-wrapper']}>
@@ -21,18 +43,31 @@ export default function Footer() {
       </div>
       <div className={styles['links-container']}>
         <div className={styles['left']}>
-          <p>Text 1</p>
-          <p>Text 2</p>
+          {homeLink}
+          {!isAuthenticated ?
+            (
+              <>
+                <Link href="/signup" passHref >
+                  <p>Sign Up</p>
+                </Link>
+                <Link href="/login" passHref >
+                  <p>Log In</p>
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link href="/" passHref >
+                  <p onClick={() => handleLogout()}>Log Out</p>
+                </Link>
+              </>)
+          }
         </div>
         <div className={styles['right']}>
-        <Link href="/" passHref >
+          <Link href="/" passHref >
             <p onClick={() => dispatch(toggle({ section: 'about', open: false }))}>About Us</p>
           </Link>
           <Link href="/" passHref >
             <p onClick={() => dispatch(toggle({ section: 'contact', open: false }))}>Contact Us</p>
-          </Link>
-          <Link href="/" passHref >
-            <p onClick={() => dispatch(toggle({ section: 'landing', open: false }))}>Home</p>
           </Link>
         </div>
       </div>
