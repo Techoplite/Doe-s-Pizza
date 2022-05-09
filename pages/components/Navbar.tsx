08e2e994551e73e08db4617a9d528848d511bb77
Techoplite <oriccimirko@gmail.com>
2022-05-09 17:57:28 +0100

    Add nav links to desktop device

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -2,13 +2,15 @@ import Image from 'next/image'
 import React from 'react'
 import { toggle } from '../../redux/menu/menuSlice'
 import styles from "../../styles/Navbar.module.scss";
-import { useAppDispatch } from '../../redux/hooks';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import Menu from './Menu';
 import NavBackground from './NavBackground';
+import Link from 'next/link';
 
 
 export default function Navbar() {
   const dispatch = useAppDispatch()
+  const isAuthenticated = useAppSelector(state => state.auth.isAuthenticated)
   return (
     <div>
       <Menu />
@@ -22,6 +24,26 @@ export default function Navbar() {
             height={30}
             layout="fixed"
           />
+        </div>
+        <div className={styles['menu-links']}>
+          <li onClick={() => dispatch(toggle({ section: 'popular-pizzas', open: false }))}>Popular Pizzas</li>
+          <li onClick={() => dispatch(toggle({ section: 'about', open: false }))}>About Us</li>
+          <li onClick={() => dispatch(toggle({ section: 'contact', open: false }))}>Contact Us</li>
+        </div>
+        <div className={`${styles['menu-links']} ${styles['auth-links']}`}>
+          {!isAuthenticated ?
+            <>
+              <Link href="/login" passHref >
+                <li>Log In</li>
+              </Link>
+              <Link href="/signup" passHref >
+                <li className={styles['sign-up']}>Sign Up</li>
+              </Link>
+
+            </>
+            :
+            <li>Log Out</li>
+          }
         </div>
         <div
           className={styles.hamburger}
