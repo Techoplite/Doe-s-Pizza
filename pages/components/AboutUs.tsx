c605e174f7f8d165f89a743899cb3ab859296696
Techoplite <oriccimirko@gmail.com>
2022-05-10 13:23:34 +0100

    Add background shapes on mobile and tablet

--- pages/components/AboutUs.tsx
+++ pages/components/AboutUs.tsx
@@ -14,33 +14,57 @@ export default function AboutUs() {
       <div className={styles['main-content']}>
         <h1>About Us</h1>
         <p data-aos="fade-up">At Doe’s Pizza we do things properly.</p>
-        <div className={styles['flex-container']}>
+        <div className={styles['flex-container']} data-aos="fade-up">
           <div className={styles['img-wrapper']}>
             <Image
-              data-aos="fade-up"
               src="/pizza-and-oven.jpg"
               alt="A pizza in front of a wood fired oven"
               width={255.65}
               height={141.82}
+              priority={true}
               layout="responsive"
             />
           </div>
-          <p data-aos="fade-up">Our wood fired oven will give your pizza a puffy crust and make it taste the way it should.</p>
+          <div className={styles['shaped-container']}>
+            <div className={styles['img-wrapper']}>
+              <Image
+                src="/background-shape.png"
+                alt="A pizza in front of a wood fired oven"
+                width={210}
+                height={141.82}
+                priority={true}
+                layout="responsive"
+              />
+            </div>
+            <p className={styles['text-on-img']}>Our wood fired oven will give your pizza a puffy crust and make it taste the way it should.</p>
+          </div>
         </div>
-        <div className={styles['flex-container']}>
-          <div className={styles['img-wrapper']}>
+        <div className={styles['flex-container']} data-aos="fade-up">
+          <div className={`${styles['img-wrapper']} `}>
             <Image
-              data-aos="fade-up"
               src="/ingredients.jpg"
               alt="A pizza in front of a wood fired oven"
               width={210}
               height={141.82}
+              priority={true}
               layout="responsive"
             />
           </div>
-          <p data-aos="zoom-up" data-aos-anchor-placement="top-bottom">
-            We use only the best ingredients to deliver the best product.
-          </p>
+          <div className={styles['shaped-container']}>
+            <div className={styles['img-wrapper']}>
+              <Image
+                src="/background-shape.png"
+                alt="A pizza in front of a wood fired oven"
+                width={210}
+                height={141.82}
+                priority={true}
+                layout="responsive"
+              />
+            </div>
+            <p className={styles['text-on-img']}>
+              We use only the best ingredients to deliver the best product.
+            </p>
+          </div>
         </div>
       </div>
     </section>
