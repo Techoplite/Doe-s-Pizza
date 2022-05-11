4325dd643dcdb4f4e1f58c9dbdc5bcbe11c40e24
Techoplite <oriccimirko@gmail.com>
2022-05-11 07:15:19 +0100

    Fix links in desktop navbar

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -6,11 +6,29 @@ import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import Menu from './Menu';
 import NavBackground from './NavBackground';
 import Link from 'next/link';
+import { setIsAuthenticated } from '../../redux/auth/authSlice';
+import { resetBooking } from '../../redux/booking/bookingSlice';
+import { resetOrder } from '../../redux/order/orderSlice';
+import { resetYourOrders } from '../../redux/yourOrders/yourOrdersSlice';
 
 
 export default function Navbar() {
   const dispatch = useAppDispatch()
   const isAuthenticated = useAppSelector(state => state.auth.isAuthenticated)
+  const handleLogout = () => {
+    // Should reset every single store
+    dispatch(toggle({ section: 'landing', open: false }))
+    dispatch(setIsAuthenticated({
+      isAuthenticated: false, credentials: {
+        username: '',
+        password: ''
+      }
+    }))
+    dispatch(resetBooking())
+    dispatch(resetOrder())
+    dispatch(resetYourOrders())
+  }
+
   return (
     <div>
       <Menu />
@@ -26,9 +44,15 @@ export default function Navbar() {
           />
         </div>
         <div className={styles['menu-links']}>
-          <li onClick={() => dispatch(toggle({ section: 'popular-pizzas', open: false }))}>Popular Pizzas</li>
-          <li onClick={() => dispatch(toggle({ section: 'about', open: false }))}>About Us</li>
-          <li onClick={() => dispatch(toggle({ section: 'contact', open: false }))}>Contact Us</li>
+          <Link href="/" passHref >
+            <li onClick={() => dispatch(toggle({ section: 'popular-pizzas', open: false }))}>Popular Pizzas</li>
+          </Link>
+          <Link href="/" passHref >
+            <li onClick={() => dispatch(toggle({ section: 'about', open: false }))}>About Us</li>
+          </Link>
+          <Link href="/" passHref >
+            <li onClick={() => dispatch(toggle({ section: 'contact', open: false }))}>Contact Us</li>
+          </Link>
         </div>
         <div className={`${styles['menu-links']} ${styles['auth-links']}`}>
           {!isAuthenticated ?
@@ -42,7 +66,9 @@ export default function Navbar() {
 
             </>
             :
-            <li>Log Out</li>
+            <Link href="/" passHref >
+              <li onClick={() => handleLogout()}>Log Out</li>
+            </Link>
           }
         </div>
         <div
