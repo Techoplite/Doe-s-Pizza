86b412fa8dad0bb25f2898e2370fe4aef9f90f02
Techoplite <oriccimirko@gmail.com>
2022-05-17 10:29:51 +0100

    Fix issue in footer sections link tests

--- cypress/integration/footer.js
+++ cypress/integration/footer.js
@@ -1,4 +1,4 @@
-import { devices, hasHamburger, sectionsOtherThanLanding } from "../support/global_variables"
+import { devices, sectionsOtherThanLanding } from "../support/global_variables"
 const test = (device) =>
     describe(`${device.name}: ${device.orientation}`, () => {
         beforeEach(() => {
@@ -21,9 +21,12 @@ const test = (device) =>
                 it.only(`scrolls to "${s.name}" when "${s.name}" is clicked`, () => {
                     cy.get('[data-test=footer]').scrollIntoView()
                     cy.wait(800)
-                    cy.get(`[data-test=${s.dataTest}`).then($section => {
-                        cy.isNotInViewport($section)
-                    })
+                    if (s.name !== 'Contact Us') {
+                        // "Contact Us" sections will always be in viewport when "Footer is displaying", so needs to be excluded from this check
+                        cy.get(`[data-test=${s.dataTest}`).then($section => {
+                            cy.isNotInViewport($section)
+                        })
+                    }
                     cy.get(`[data-test=footer_${s.dataTest}_link]`).click()
                     cy.get(`[data-test=${s.dataTest}`).then($section => {
                         cy.isInViewport($section)
